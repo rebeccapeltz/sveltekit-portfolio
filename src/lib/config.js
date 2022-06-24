f8afb9a879f3c9cfd83ba7391dd19a3deb004f8d
Rebeccca Peltz <rebeccapeltz@gmail.com>
2022-06-24 12:42:13 -0700

    personalize

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,8 +1,8 @@
 export const siteTitle = 'Becky Peltz Portfolio'
 export const siteDescription = 'Built with the SvelteKit Static Blog Starter'
 export const siteURL = 'example.com'
-export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
-export const siteAuthor = 'Josh Collinsworth - find and change this text in src/lib/config.js'
+export const siteLink = 'https://github.com/rebeccapeltz/sveltekit-portfolio'
+export const siteAuthor = 'Rebecca Peltz'
 
 // Controls how many posts are shown per page on the main blog index pages
 export const postsPerPage = 10
